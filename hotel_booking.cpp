b990487f051ec742cb737fc8c749c90211f4ba8d
kiya yilma <you@example.com>
2026-01-18 19:34:32 +0300

    Add search guest feature (Kiya)

--- hotel_booking.cpp
+++ hotel_booking.cpp
@@ -22,6 +22,27 @@ int main()
 
     // ===== SECTION E: SEARCH GUEST =====
     // Kiya will work here
+    string searchName;
+cout << "Enter a name to search for patrons: ";
+cin >> searchName;
+
+bool found = false;
+for (int i = 0; i < rooms; ++i)
+{
+    if (guestNames[i] == searchName)
+    {
+        cout << "Guest found in room " << i + 1
+             << ". Reservation code: " << reservationCodes[i] << endl;
+        found = true;
+        break;
+    }
+}
+
+if (!found)
+{
+    cout << "No guest found with the given name." << endl;
+}
+
 
     return 0;
 }
